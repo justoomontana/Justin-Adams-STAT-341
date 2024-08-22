873573b67cf176c90f8e1d0fd5cb5d87d46c376a
Justin Adams <144487569+justoomontana@users.noreply.github.com>
2024-08-22 16:25:36 -0400

    Add files via upload

---
+++ august 22, 2024 r  .R
@@ -0,0 +1,14 @@
+#Author: Justin Adams, Date:08/22/24, Purpose:Test the measures of descriptive stats
+
+#How to calculate standard deviation in R 
+X <- c(1,2,3,4,5)
+ sd(X)
+ 
+ #How to calculate mean in R
+ mean(X)
+ 
+ #How to calculate median in R 
+ median(x)
+ 
+ #How to calculate mode in R
+ 
