293ef6e814b26c264ae2f6ae0e68239ba173d3b3
Justin Adams <144487569+justoomontana@users.noreply.github.com>
2024-08-29 16:22:19 -0400

    Add files via upload

---
+++ stat assignment 3.R
@@ -0,0 +1,15 @@
+1. Create 1000 random numbers and store them in a variable called variable1
+
+2. calculate standard deviation, mean, and median 
+
+3.save output of sd mean and median in R document along with your statement name and doc
+
+variable1 <- runif(1000)
+sd_variable1 <- sd(variable1)
+mean_variable1 <- mean(variable1)
+mean_variable1 <- mean(variable1)
+1] 0.2915629
+> mean(variable1)
+[1] 0.5067384
+> mean(variable1)
+[1] 0.5067384
